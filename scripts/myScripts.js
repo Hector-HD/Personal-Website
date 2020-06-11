6dde6761a62a424337edfec0e1f1d0e5909eef1d
hduenas1 <hduenas1@asu.edu>
2020-06-11 12:29:50 -0700

    Fixed translateX pos

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,10 +1,21 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-list');
+    const navLinks = document.querySelectorAll('.nav-list li');
 
+    //toggle now
     burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
-    });
+
+        //Animate Links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = 'navLinkFade 0.5s ease forward ${index / 7 + 1.5}s';
+            }
+        });
+    });   
 }
 
 
