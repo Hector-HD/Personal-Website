e88397e1041a6816b1cedcf11ee6a0e70d12064d
Hector Duenas <hectorduenas.hd@gmail.com>
2020-06-11 13:26:18 -0700

    navigation is doen with responsive js function

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -4,17 +4,20 @@ const navSlide = () =>{
     const navLinks = document.querySelectorAll('.nav-list li');
 
     //toggle now
-    burger.addEventListener('click',()=>{
+    burger.addEventListener('click', ()=> {
         nav.classList.toggle('nav-active');
 
         //Animate Links
         navLinks.forEach((link, index)=>{
             if(link.style.animation){
-                link.style.animation = '';
+                link.style.animation = ``;
             }else{
-                link.style.animation = 'navLinkFade 0.5s ease forward ${index / 7 + 1.5}s';
+                link.style.animation = `navListFade 0.5s ease forwards ${index / 7+.5}s`;
             }
         });
+
+        //burger animation
+        burger.classList.toggle('toggle')
     });   
 }
 
